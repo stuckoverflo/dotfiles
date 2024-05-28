0ceb7dadfe711c65368f9e2d0352212163e266ba
flo <florobarotjr@gmail.com>
2024-05-28 10:27:42 +0800

    Cleanup unused files

--- git/git
+++
@@ -1,3 +0,0 @@
-git config --global diff.tool vimdiff
-git config --global difftool.prompt false
-git config --global alias.d difftool
--- mas/mas.sh
+++
@@ -1,5 +0,0 @@
-thisDir=${0%/*}
-while read p; do
-    app=$(echo $p | cut -d" " -f1)
-    mas install $app
-done <"$thisDir/apps.txt"
