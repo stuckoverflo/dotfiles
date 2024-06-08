1a6b4cb819a9a68135411637b846d695641521d8
flo <florobarotjr@gmail.com>
2024-06-08 14:43:40 +0800

    Port the trouble.nvim keymaps to v3-compatible

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -2,11 +2,15 @@ return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
   keys = {
-    { "<leader>xx", "<cmd>TroubleToggle<CR>", desc = "Open/close trouble list" },
-    { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
-    { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
-    { "<leader>xq", "<cmd>TroubleToggle quickfix<CR>", desc = "Open trouble quickfix list" },
-    { "<leader>xl", "<cmd>TroubleToggle loclist<CR>", desc = "Open trouble location list" },
-    { "<leader>xt", "<cmd>TodoTrouble<CR>", desc = "Open todos in trouble" },
+    { "<leader>xx", "<cmd>Trouble<CR>", desc = "Open/close trouble list" },
+    { "<leader>xw", "<cmd>Trouble diagnostics toggle focus=true<CR>", desc = "Open trouble workspace diagnostics" },
+    {
+      "<leader>xd",
+      "<cmd>Trouble diagnostics toggle filter.buf=0 focus=true<CR>",
+      desc = "Open trouble document diagnostics",
+    },
+    { "<leader>xq", "<cmd>Trouble quickfix toggle focus=true<CR>", desc = "Open trouble quickfix list" },
+    { "<leader>xl", "<cmd>Trouble loclist toggle focus=true<CR>", desc = "Open trouble location list" },
+    { "<leader>xt", "<cmd>Trouble todo toggle focus=true<CR>", desc = "Open todos in trouble" },
   },
 }
