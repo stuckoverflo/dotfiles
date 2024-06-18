8172d63643f9972b435b09e900e8dd73c97de9ed
flo <florobarotjr@gmail.com>
2024-06-18 12:42:09 +0800

    Readd substitute keymap descriptions

--- .config/nvim/lua/plugins/substitute.lua
+++ .config/nvim/lua/plugins/substitute.lua
@@ -6,9 +6,9 @@ return {
 
     local substitute = require("substitute")
 
-    vim.keymap.set("n", "s", substitute.operator, { noremap = true })
-    vim.keymap.set("n", "ss", substitute.line, { noremap = true })
-    vim.keymap.set("n", "S", substitute.eol, { noremap = true })
-    vim.keymap.set("x", "s", substitute.visual, { noremap = true })
+    vim.keymap.set("n", "s", substitute.operator, { noremap = true, desc = "substitute with motion" })
+    vim.keymap.set("n", "ss", substitute.line, { noremap = true, desc = "substitute line" })
+    vim.keymap.set("n", "S", substitute.eol, { noremap = true, desc = "substitute to the end of the line" })
+    vim.keymap.set("x", "s", substitute.visual, { noremap = true, desc = "substitute in visual mode" })
   end,
 }
