8ff1d1bc688a9ddbd5e29dd5db4b3c2decd2bb78
flo <florobarotjr@gmail.com>
2024-06-21 08:26:23 +0800

    chore: cleanup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,19 +7,18 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({ { import = "plugins" } }, {
--- require("lazy").setup({ { import = "plugins" }, { import = "plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,
   },
   change_detection = {
-    notify = false
+    notify = false,
   },
 })
