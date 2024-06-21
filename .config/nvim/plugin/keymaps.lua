fca55bf5ab07792bdb75cad79e5e4e81564da360
flo <florobarotjr@gmail.com>
2024-06-21 08:15:37 +0800

    feat: additional keymaps

--- .config/nvim/plugin/keymaps.lua
+++ .config/nvim/plugin/keymaps.lua
@@ -1,4 +1,3 @@
-
 local keymap = vim.keymap
 
 keymap.set("i", "jj", "<ESC>", { desc = "Exit insert mode with jj" })
@@ -8,6 +7,10 @@ keymap.set("n", "<leader> ", ":update<CR>", { desc = "Save the current buffer" }
 
 keymap.set("n", "j", "gj", { desc = "Treat long lines as break lines" })
 keymap.set("n", "k", "gk", { desc = "Treat long lines as break lines" })
+keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Center the cursor when navigating using C-d" })
+keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Center the cursor when navigating using C-u" })
+keymap.set("n", "n", "nzzzv", { desc = "Center the cursor when navigating search results" })
+keymap.set("n", "N", "Nzzzv", { desc = "Center the cursor when navigating search results" })
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
@@ -26,3 +29,13 @@ keymap.set("v", ">", ">gv", { desc = "Reselect visual block after indent" })
 keymap.set("v", "<", "<gv", { desc = "Reselect visual block after outdent" })
 
 keymap.set("n", "<leader>w", ":set wrap!<CR>", { desc = "Toggle line wrap" })
+
+keymap.set(
+  "n",
+  "<leader>s",
+  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+  { desc = "Search and replace the word under cursor" }
+)
+
+keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Smartly move highlighted lines of code" })
+keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Smartly move highlighted lines of code" })
