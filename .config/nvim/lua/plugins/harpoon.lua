b6888a5d6490ebb763413454590c881dc2be2ee9
flo <florobarotjr@gmail.com>
2024-06-21 08:23:19 +0800

    fix: the harpoon keymaps

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -11,12 +11,12 @@ return {
     vim.keymap.set("n", "<leader>hl", function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end)
-    vim.keymap.set("n", "A-p", function()
+    vim.keymap.set("n", "<M-j>", function()
       harpoon:list():prev()
-    end, { noremap = true })
-    vim.keymap.set("n", "A-n", function()
+    end, { noremap = true, desc = "harpoon: prev buffer" })
+    vim.keymap.set("n", "<M-k>", function()
       harpoon:list():next()
-    end, { noremap = true })
+    end, { noremap = true, desc = "harpoon: next buffer" })
     -- Set <space>1..<space>5 be my shortcuts to moving to the files
     for _, idx in ipairs({ 1, 2, 3, 4, 5 }) do
       vim.keymap.set("n", string.format("<space>%d", idx), function()
