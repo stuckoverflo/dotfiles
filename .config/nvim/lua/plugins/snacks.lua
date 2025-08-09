2d64f0cf10747e850a6f3c9021733c04c20a4fc5
fbarot <fbarot@apexfintechsolutions.com>
2025-08-09 19:00:23 +0800

    try out snacks.lua

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -2,24 +2,35 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
-  config = function()
-    require("snacks").setup({
-      input = {
-        enabled = true,
-        icon = " ",
-        icon_hl = "SnacksInputIcon",
-        icon_pos = "left",
-        prompt_pos = "title",
-        win = { style = "input" },
-        expand = true,
-      },
-      notifier = { enabled = true, timeout = 3000 },
-      scroll = { enabled = true },
-      -- Add your configuration here
-      -- For example:
-      -- default_keymaps = true,
-      -- highlight = "Visual",
-      -- border = "rounded",
-    })
-  end,
+  ---@type snacks.Config
+  opts = {
+    input = {
+      enabled = true,
+      icon = " ",
+      icon_hl = "SnacksInputIcon",
+      icon_pos = "left",
+      prompt_pos = "title",
+      win = { style = "input" },
+      expand = true,
+    },
+    notifier = { enabled = true },
+    scratch = { enabled = true },
+    scroll = { enabled = true },
+  },
+  keys = {
+    {
+      "<leader>.",
+      function()
+        Snacks.scratch()
+      end,
+      desc = "Toggle Scratch Buffer",
+    },
+    {
+      "<leader>S",
+      function()
+        Snacks.scratch.select()
+      end,
+      desc = "Select Scratch Buffer",
+    },
+  },
 }
