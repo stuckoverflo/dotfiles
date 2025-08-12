d2fb3f5fa479fec4002b649feebb79ea9e2cca09
fbarot <fbarot@apexfintechsolutions.com>
2025-08-12 12:09:28 +0800

    Add snacks keybinds and dashboard

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,9 +1,25 @@
 return {
   "folke/snacks.nvim",
   priority = 1000,
+  
   lazy = false,
   ---@type snacks.Config
   opts = {
+    dashboard = {
+      enabled = true,
+      preset = {
+        header = [[
+          ███████╗██╗      ██████╗  
+       ██╗██╔════╝██║     ██╔═══██╗ 
+       ╚═╝█████╗  ██║     ██║   ██║ 
+       ██╗██╔══╝  ██║     ██║   ██║ 
+       ╚═╝██║     ███████╗╚██████╔╝ 
+          ╚═╝     ╚══════╝ ╚═════╝  
+        ]] .. vim.version().major .. '.' .. vim.version().minor .. '.' .. vim.version().patch,
+        keys = {},
+      },
+    },
+    explorer = {},
     input = {
       enabled = false,
       icon = " ",
@@ -14,23 +30,24 @@ return {
       expand = true,
     },
     notifier = { enabled = true },
+    picker = {
+      enabled = true,
+      cwd_bonus = true,
+    },
     scratch = { enabled = true },
     scroll = { enabled = true },
   },
   keys = {
-    {
-      "<leader>.",
-      function()
-        Snacks.scratch()
-      end,
-      desc = "Toggle Scratch Buffer",
-    },
-    {
-      "<leader>S",
-      function()
-        Snacks.scratch.select()
-      end,
-      desc = "Select Scratch Buffer",
-    },
+    { '<leader>e', function() Snacks.picker.explorer() end, desc = 'Explorer' },
+    { "<leader>.", function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+    { "<leader>S", function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+    { "<leader>ff", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+    { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Find in Buffers" },
+    { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
+    { "<leader>fw", function() Snacks.picker.grep() end, desc = "Grep" },
+    { "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
+    { '<leader>sd', function() Snacks.picker.diagnostics() end, desc = 'Diagnostics' },
+    { "<leader>vc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
+    { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
   },
 }
