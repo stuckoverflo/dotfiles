96eb35a3c417c83410d9e539330138ef9e5e559c
fbarot <fbarot@apexfintechsolutions.com>
2025-08-14 22:31:59 +0800

    Update config

--- .config/nvim/lua/plugins/copilotchat.lua
+++ .config/nvim/lua/plugins/copilotchat.lua
@@ -1,6 +1,7 @@
 return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
     dependencies = {
       { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
