233150d4e240fc529d509b5c622ad6119b1863f0
fbarot <fbarot@apexfintechsolutions.com>
2025-08-15 20:37:37 +0800

    Re-add noice

---
+++ .config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,44 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {
+    messages = {
+      enabled = true,
+    },
+    popupmenu = {
+      enabled = true,
+    },
+    notify = {
+      enabled = true,
+    },
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+      },
+    },
+    presets = {
+      bottom_search = false,
+      command_palette = false,
+      long_message_to_split = true,
+      lsp_doc_border = false,
+    },
+    routes = {
+      {
+        filter = { event = "msg_show", kind = { "shell_out", "shell_err", "shell_ret" } },
+        view = "split",
+        opts = {
+          level = "info",
+          skip = false,
+          replace = false,
+        },
+      },
+    },
+  },
+  config = true,
+}
