56247b9f3d36646d101c4c1dcccc7fa0ac66e8ec
fbarot <fbarot@apexfintechsolutions.com>
2025-08-23 11:39:41 +0800

    use the other copilot
    this fixes the issue wherein copilot doesn't respect the filetype-based
    enabling of `zbirenbaum/copilot.lua` plugin

--- .config/nvim/lua/plugins/copilotchat.lua
+++ .config/nvim/lua/plugins/copilotchat.lua
@@ -3,7 +3,8 @@ return {
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "canary",
     dependencies = {
-      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      -- { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "zbirenbaum/copilot.lua" }, -- or zbirenbaum/copilot.lua
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
     build = "make tiktoken", -- Only on MacOS or Linux
