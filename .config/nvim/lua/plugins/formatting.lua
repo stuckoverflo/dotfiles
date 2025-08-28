6f264c3d8c78040544814d1673c15e16cd03457a
fbarot <fbarot@apexfintechsolutions.com>
2025-08-28 20:02:20 +0800

    Remove formatters that are unused

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -5,20 +5,19 @@ return {
     local conform = require("conform")
 
     conform.setup({
+      formatters = {
+        black = {
+          prepend_args = { "--fast" },
+        },
+      },
       formatters_by_ft = {
         css = { "prettier" },
         go = { "gofumpt" },
-        graphql = { "prettier" },
-        html = { "prettier" },
-        javascript = { "prettier" },
-        javascriptreact = { "prettier" },
         json = { "prettier" },
         lua = { "stylua" },
         markdown = { "prettier" },
         python = { "isort", "black" },
         terraform = { "terraform_fmt" },
-        typescript = { "prettier" },
-        typescriptreact = { "prettier" },
         yaml = { "prettier" },
       },
       format_on_save = function(bufnr)
