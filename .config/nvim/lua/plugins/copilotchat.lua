0022aa57e05c08df014aea1e1a38dd889620fa8e
fbarot <fbarot@apexfintechsolutions.com>
2025-08-28 19:53:18 +0800

    Remove canary branch

--- .config/nvim/lua/plugins/copilotchat.lua
+++ .config/nvim/lua/plugins/copilotchat.lua
@@ -1,7 +1,6 @@
 return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
     dependencies = {
       -- { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
       { "zbirenbaum/copilot.lua" }, -- or zbirenbaum/copilot.lua
